12a3a8084888eaf7dcda3b92c41d7cb18509f36c
Maxime REVEL <maxime.revel@360learning.com>
2023-12-04 18:23:46 +0100

    Solve Day 1 part 1

--- advent-23-day-1.playground/Pages/Solution.xcplaygroundpage/Contents.swift
+++ advent-23-day-1.playground/Pages/Solution.xcplaygroundpage/Contents.swift
@@ -1,14 +1,13 @@
 /// The interesting file is this one
 import Foundation
 
-resolvePartOne(Launcher.example)
+resolvePartOne(Launcher.puzzleInput)
 
 func resolvePartOne(_ input: [String]) {
-    
-    // TODO: Solve
-    
+    let result = input.reduce(0, { $0 + addFirstAndLastNumber(of: $1) })
+
     D.log(D.solution, newlines: true)
-    print()
+    print(result)
 }
 
 func resolvePartTwo(_ input: [String]) {
@@ -17,4 +16,30 @@ func resolvePartTwo(_ input: [String]) {
     
     D.log(D.solution, newlines: true)
     print()
+}
+
+}
+
+// MARK: Part one
+
+func getFirstNumber(of line: String) -> Int? {
+    return line.first(where: { $0.isNumber })?.asInt
+}
+
+func getLastNumber(of line: String) -> Int? {
+    return line.last(where: { $0.isNumber })?.asInt
+}
+
+func addFirstAndLastNumber(of line: String) -> Int {
+    let first: Int = getFirstNumber(of: line) ?? 0
+    let last: Int = getLastNumber(of: line) ?? 0
+    let result = 10 * first + last
+    D.log(result)
+    return result
+}
+
+extension Character {
+    var asInt: Int {
+        return Int(String(self)) ?? 0
+    }
 }
