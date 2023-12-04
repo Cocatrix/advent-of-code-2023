432e5b43f3aa8f43da06e9b28f3be549e2600460
Maxime REVEL <maxime.revel@360learning.com>
2023-12-04 18:31:11 +0100

    Solve Day 2 part 2

--- advent-23-day-2.playground/Pages/Solution.xcplaygroundpage/Contents.swift
+++ advent-23-day-2.playground/Pages/Solution.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 /// The interesting file is this one
 import Foundation
 
-resolvePartOne(Launcher.puzzleInput)
+resolvePartTwo(Launcher.puzzleInput)
 
 func resolvePartOne(_ input: [String]) {
     
@@ -16,10 +16,13 @@ func resolvePartOne(_ input: [String]) {
 
 func resolvePartTwo(_ input: [String]) {
     
-    // TODO: Solve
+    let result = input
+        .map { parse(line: $0) }
+        .map { mapToMinimumReveal(reveals: $0.reveals) }
+        .reduce(0, { $0 + power(of: $1) })
     
     D.log(D.solution, newlines: true)
-    print()
+    print(result)
 }
 
 typealias Reveal = (red: Int, green: Int, blue: Int)
@@ -58,4 +61,24 @@ func parseColors(from set: String) -> Reveal {
 
 func isAboveThreshold(reveal: Reveal) -> Bool {
     return reveal.red > 12 || reveal.green > 13 || reveal.blue > 14
+}
+
+func mapToMinimumReveal(reveals: [Reveal]) -> Reveal {
+    var minimum: Reveal = (0, 0, 0)
+    reveals.forEach { reveal in
+        if reveal.red > minimum.red {
+            minimum.red = reveal.red
+        }
+        if reveal.green > minimum.green {
+            minimum.green = reveal.green
+        }
+        if reveal.blue > minimum.blue {
+            minimum.blue = reveal.blue
+        }
+    }
+    return minimum
+}
+
+func power(of reveal: Reveal) -> Int {
+    return reveal.red * reveal.green * reveal.blue
 }
